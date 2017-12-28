c496eeb5bb68d84ae227c573d299f621e2d976ce
Kamil Kostyszyn <inkwizytor89@gmail.com>
2017-12-28 16:01:08 +0100

    sample test

---
+++ src/test/java/test/FirstTestCase.java
@@ -0,0 +1,28 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * http://toolsqa.com/selenium-tutorial/
+ */
+public class FirstTestCase {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new ChromeDriver();
+
+        //Launch the Online Store Website
+        driver.get("http://www.store.demoqa.com");
+
+        // Print a Log In message to the screen
+        System.out.println("Successfully opened the website www.Store.Demoqa.com");
+
+        //Wait for 5 Sec
+        Thread.sleep(5000);
+
+        // Close the driver
+        driver.quit();
+    }
+}
